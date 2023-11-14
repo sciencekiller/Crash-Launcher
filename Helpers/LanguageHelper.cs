e0714f666e8964b4642702fa9313a92ffc6be723
添宇 程 <sciencekill@outlook.com>
2023-11-14 20:09:53 +0800

    Update

--- Helpers/LanguageHelper.cs
+++ Helpers/LanguageHelper.cs
@@ -1,7 +1,14 @@
-﻿namespace Crash_Launcher.Helpers
+﻿using System.Net;
+using System.Net.Security;
+
+namespace Crash_Launcher.Helpers
 {
     class LanguageHelper
     {
         internal static string systemLanguage = System.Globalization.CultureInfo.InstalledUICulture.Name;
+        internal static void getSupportedLanguages()
+        {
+            
+        }
     }
 }
